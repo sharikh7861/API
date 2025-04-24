bc573370b2580629e7fe454b87c3dc26842e5a23
samsungtms99 <samsungtms99@gmail.com>
2025-04-24 20:14:05 +0530

    first commit

---
+++ src/main/java/com/hunar/api/HunarApiApplication.java
@@ -0,0 +1,13 @@
+package com.hunar.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HunarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HunarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hunar/api/HunarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunar.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HunarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
